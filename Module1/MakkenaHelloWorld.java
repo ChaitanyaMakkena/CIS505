12fa98a7d7152d8835a6559650e962a27a7bae08
ChaitanyaMakkena <152934475+ChaitanyaMakkena@users.noreply.github.com>
2023-12-04 20:34:59 -0600

    Module1

---
+++ Module1/MakkenaHelloWorld.java
@@ -0,0 +1,12 @@
+ @author Chaitanya
+ *
+ */
+public class MakkenaHelloWorld {
+
+	static String firstName = "Chaitanya";
+	static String lastName= "Makkena";
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World from "+ firstName +" "+lastName);
+	}
+}
